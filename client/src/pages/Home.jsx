ca632a848ac5a5510bf3cfb60066082af402f3ba
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-14 15:16:59 +0900

    finishing campaign card but have date error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import { useStateContext } from '../context'
+import React, { useState, useEffect } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Home = () => {
-
   const [isLoading, setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
 
@@ -13,19 +13,21 @@ const Home = () => {
     const data = await getCampaigns();
     setCampaigns(data);
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
     if (contract) {
       fetchCampaigns();
     }
-
-  }, [address, contract])
-  
+  }, [address, contract]);
 
   return (
-    <div>Home</div>
-  )
-}
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
 
-export default Home
+export default Home;
