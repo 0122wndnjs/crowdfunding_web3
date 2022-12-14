31a4b6f814678cee23b99f251090529d9426be22
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-14 13:58:54 +0900

    connecting metamask and contract success

---
+++ client/src/context/index.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xfeA4c0F171d57b754b844e2e17f48cAA73f3fe92"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, // owner
+        form.title, // title
+        form.description, // description
+        form.target,
+        new Date(form.deadline).getTime() / 1000, // deadline
+        form.image,
+      ]);
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{ address, contract, connect, createCampaign: publishCampaign }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
