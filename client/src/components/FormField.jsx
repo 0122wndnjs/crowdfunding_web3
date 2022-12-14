c435d32fe3c74929f73d7e006536ba3ff7687b42
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-14 11:49:19 +0900

    fully functioning create campaign page

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -19,7 +19,7 @@ const FormField = ({
         <textarea
           required
           value={value}
-          onchange={handleChange}
+          onChange={handleChange}
           rows={10}
           placeholder={placeholder}
           className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
@@ -28,7 +28,7 @@ const FormField = ({
         <input
           required
           value={value}
-          onchange={handleChange}
+          onChange={handleChange}
           type={inputType}
           step="0.1"
           placeholder={placeholder}
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -19,7 +19,15 @@ const CreateCampaign = () => {
     image: "",
   });
 
-  const handleSubmit = () => {};
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(form);
+  };
 
   return (
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
@@ -40,14 +48,14 @@ const CreateCampaign = () => {
             placeholder="John Doe"
             inputType="text"
             value={form.name}
-            handlechange={() => {}}
+            handleChange={(e) => handleFormFieldChange("name", e)}
           />
           <FormField
             labelName="Campaign Title *"
             placeholder="Write a title"
             inputType="text"
             value={form.title}
-            handlechange={() => {}}
+            handleChange={(e) => handleFormFieldChange("title", e)}
           />
         </div>
         <FormField
@@ -55,7 +63,7 @@ const CreateCampaign = () => {
           placeholder="Write your story"
           isTextArea
           value={form.description}
-          handlechange={() => {}}
+          handleChange={(e) => handleFormFieldChange("description", e)}
         />
 
         <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
@@ -74,23 +82,31 @@ const CreateCampaign = () => {
             placeholder="ETH 0.50"
             inputType="text"
             value={form.target}
-            handlechange={() => {}}
+            handleChange={(e) => handleFormFieldChange("target", e)}
           />
           <FormField
             labelName="End Date *"
             placeholder="End Date"
             inputType="date"
             value={form.deadline}
-            handlechange={() => {}}
+            handleChange={(e) => handleFormFieldChange("deadline", e)}
           />
+        </div>
 
-          <div className="flex justify-center items-center mt-[40px]">
-            <CustomButton
-              btnType="submit"
-              title="Submit new campaign"
-              styles="bg-[#1dc071]"
-            />
-          </div>
+        <FormField
+          labelName="Campaign image *"
+          placeholder="Place image URL of your campaign"
+          inputType="url"
+          value={form.image}
+          handleChange={(e) => handleFormFieldChange("image", e)}
+        />
+
+        <div className="flex justify-center items-center mt-[40px]">
+          <CustomButton
+            btnType="submit"
+            title="Submit new campaign"
+            styles="bg-[#1dc071]"
+          />
         </div>
       </form>
     </div>
